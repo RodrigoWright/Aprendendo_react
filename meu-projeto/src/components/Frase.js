8b6a9afd81b06bb1f2b0acc154290aeae0cc3ad5
RodrigoWright <wright.rodrigo380@gmail.com>
2022-06-29 13:16:11 -0300

    final aula 6/ adicionando estilos aos componentes

--- meu-projeto/src/components/Frase.js
+++ meu-projeto/src/components/Frase.js
@@ -1,8 +1,10 @@
+import styles from './Frase.module.css'
+
 function Frase() {
 
     return(
-        <div>
-            <p>Componente com uma frase</p>
+        <div className = {styles.fraseContainer}>
+            <p className = {styles.fraseContent}>Isso é uma frase!!</p>
         </div>
     )
 }
